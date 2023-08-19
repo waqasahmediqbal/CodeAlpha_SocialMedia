b8d9d47e9dde39763f663906f66d788ceafd8b42
waqasahmediqbal <wa274116@gmail.com>
2023-08-20 00:54:19 +0500

    error solved

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -18,7 +18,7 @@ const Profile = ({
 }) => {
   useEffect(() => {
     getProfileById(match.params.id);
-  }, [getProfileById, match.params.id]);
+  }, [getProfileById]);
   return (
     <Fragment>
       {profile === null || loading ? (
